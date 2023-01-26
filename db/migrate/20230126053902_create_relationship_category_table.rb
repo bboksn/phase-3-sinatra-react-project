aac362d6e83a8b23657551aba33b63cbae1777e3
Ben <bboksner@LAPTOP-0U0LFUE0.localdomain>
2023-01-26 02:21:38 -0500

    ran first migration to create relation table

--- db/migrate/20230126053902_create_relationship_category_table.rb
+++ db/migrate/20230126053902_create_relationship_category_table.rb
@@ -1,7 +1,8 @@
 class CreateRelationshipCategoryTable < ActiveRecord::Migration[6.1]
   def change
     create_table :relationship_categories do |t|
-      
+      t.string :relation
+      t.string :desc
     end 
   end
 end
