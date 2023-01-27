2a4037fac8085fb278d11d6f9086e0f3fc15549b
Ben <bboksner@LAPTOP-0U0LFUE0.localdomain>
2023-01-26 20:12:41 -0500

    added routes to query by relation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,21 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
+  #read
   get "/contacts" do
     Contact.all.to_json(include: :relationship)
+  end
+  get "/relationships" do
+    Relationship.all.to_json(include: :contacts)
+  end
+  get "/relationships/:relation" do
+    Relationship.where(relation: params[:relation]).to_json(include: :contacts)
+  end
+  #create
+  get "/contacts/new" do
+  end
+  get "/relationships/new" do
+    Relationship.all.to_json(include: :contacts)
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
 puts "🌱 Seeding spices..."
 
 # Seed your database here
-r= Relationship.create(relation:"friend",desc:"cool")
-Contact.create(pfp_url:"https://hi",f_name:"ben",l_name:"jamin",phone_number:1234567890,relationship_id:r.id)
+friend= Relationship.create(relation:"friend",desc:"cool")
+Contact.create(pfp_url:"https://hi",f_name:"ben",l_name:"jamin",phone_number:1234567890,relationship_id:friend.id)
+Contact.create(pfp_url:"https://image",f_name:"shmen",l_name:"jamin",phone_number:1334567890,relationship_id:friend.id)
+coworker = Relationship.create(relation:"coworker",desc:"the people I work with")
+Contact.create(pfp_url:"https://image",f_name:"john",l_name:"doe",phone_number:5555555555,relationship_id:coworker.id)
+
 
 puts "✅ Done seeding!"
