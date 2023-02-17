633c5b710617fb3b9faeb34050139875866a20f5
bboks <bboksner@gmail.com>
2023-02-16 21:10:41 -0500

    patch controller working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -42,24 +42,14 @@ class ApplicationController < Sinatra::Base
     c= Contact.find(params[:id])
     c.destroy
   end
-#patch
-patch "contacts/:id" do 
-  c= Contact.find(params[:id]).update(
-    pfp_url: params[:pfp_url],
-    f_name: params[:f_name],
-    l_name: params[:l_name],
-    phone_number: params[:phone_number],
-    relationship_id: params[:relationship_id]
-  )
-  c.to_json
+#patch 
+patch '/contacts/:id' do
+  c = Contact.find(params[:id])
+  c.pfp_url = params[:pfp_url]
+  c.f_name = params[:f_name]
+  c.l_name = params[:l_name]
+  c.phone_number = params[:phone_number]
+  c.relationship_id = params[:relationship_id]
+  c.save
 end
-#patch "/contacts/:id" do
- # c= Contact.find(params[:id])
-  #c.pfp_url = params[:pfp_url]
-  #c.f_name = params[:f_name]
-  #c.l_name = params[:l_name]
-  #c.phone_number = params[:phone_number]
-  #c.relationship_id params[:relationship_id]
-  #return c.to_json
-#end
 end
